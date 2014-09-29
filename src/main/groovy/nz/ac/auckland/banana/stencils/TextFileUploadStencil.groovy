25284454d553d75d844a50bd22fd959b10985fe1
deffer <irina.benediktovich@gmail.com>
2014-09-30 10:27:30 +1300

    refactoring to stencils

--- src/main/groovy/nz/ac/auckland/banana/stencils/TextFileUploadStencil.groovy
+++ src/main/groovy/nz/ac/auckland/banana/stencils/TextFileUploadStencil.groovy
@@ -1,39 +1,66 @@
 package nz.ac.auckland.banana.stencils
 
 import nz.ac.auckland.stencil.Stencil
+import nz.ac.auckland.util.JacksonHelper
 import org.apache.commons.fileupload.FileItem
 import org.apache.commons.fileupload.disk.DiskFileItemFactory
 import org.apache.commons.fileupload.servlet.ServletFileUpload
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-
+/**
+ * Stencil for uploading (accepting) small text files.
+ * Returns content as string (descendants need to implement one abstract method accepting the content)
+ */
 abstract class TextFileUploadStencil implements Stencil{
+	private Logger log = LoggerFactory.getLogger(getClass())
 
-	int maxFileSize = 1024*1024*5
+	int maxSize = 1024*1024*10
 
 	abstract void acceptFile(String name, String body, HttpServletResponse response);
+
+	/**
+	 * Override to provide different max file size.
+	 * Note that content is returned as a string, therefore size shouldn't be very big.
+	 * @return file size in bytes. Anything bigger will be rejected with exception from apache commons.
+	 */
+	public int getMaxFileSize(){
+		return maxSize
+	}
+
+	/**
+	 * Override to manually handle errors.
+	 * Currently only one error is supported: not a multi part request. Also exceptions.
+	 * @param message
+	 * @param response
+	 */
+	@SuppressWarnings("static-method")
+	public void handleError(String message, HttpServletResponse response){
+		response.setHeader("Content-Type", "application/json");
+		response.setContentType("application/json");
+		response.writer.write(JacksonHelper.serialize([error: message]));
+	};
+
 
 	void render(HttpServletRequest request, HttpServletResponse response, Map<String, String> pathParameters) {
 		// Check that we have a file upload request
 		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter( );
 		if( !isMultipart ){
-			response.setHeader("Content-Type", "application/json");
-			response.setContentType("text/html");
-			response.writer.write("{\"error\":\"Not a mulitpart\"}");
-			return;
+			handleError("Not a multipart", response);
+			return
 		}
 
-		// Create a factory for disk-based file items
-		//DiskFileItemFactory factory = new DiskFileItemFactory(maxFileSize*2, yourTempDirectory);
-		DiskFileItemFactory factory = new DiskFileItemFactory(maxFileSize*2, new File("c:\\temp"));
+		response.setContentType("text/html");
+
+		// Create a factory for disk-based file items. Technically this code will never need to create a file,
+		//    but factory is required by API.
+		DiskFileItemFactory factory = new DiskFileItemFactory(getMaxFileSize()*2, new File(System.getProperty("java.io.tmpdir")));
 
 		// Create a new file upload handler
 		ServletFileUpload upload = new ServletFileUpload(factory);
-
 		upload.setSizeMax( maxFileSize ); // maximum file size to be uploaded.
 
 		try{
@@ -60,14 +87,15 @@ abstract class TextFileUploadStencil implements Stencil{
 
 					String body = fi.inputStream.text
 
-					print "File $fileName size ${sizeInBytes}b"
-					println body.take(300)
+					log.debug("Accepted file $fileName size ${sizeInBytes}b")
+					log.trace(body.take(300).toString())
 					acceptFile(fileName, body, response)
 				}
 			}
 
 		}catch(Exception ex) {
-			System.out.println(ex);
+			log.error(ex.getMessage(), ex);
+			handleError(ex.getMessage(), response);
 		}
 	}
 }
---
+++ src/main/groovy/nz/ac/auckland/banana/stencils/UploadBookmarksStencil.groovy
@@ -0,0 +1,93 @@
+package nz.ac.auckland.banana.stencils
+
+import nz.ac.auckland.banana.services.BookmarksStore
+import nz.ac.auckland.banana.services.UserStore
+import nz.ac.auckland.stencil.Path
+import nz.ac.auckland.util.JacksonHelper
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.nodes.Element
+import org.jsoup.select.Elements
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import javax.inject.Inject
+import javax.servlet.http.HttpServletResponse
+
+@Path("/upload")
+class UploadBookmarksStencil extends TextFileUploadStencil {
+
+	private static final Logger log = LoggerFactory.getLogger(UploadBookmarksStencil)
+
+	int maxFileSize = 1024 * 1024 * 5
+
+	@Inject UserStore userStore
+	@Inject BookmarksStore bookmarksStore
+
+	void acceptFile(String name, String body, HttpServletResponse response){
+		response.setHeader("Content-Type", "application/json");
+		response.setContentType("text/html");
+		//PrintWriter out = response.getWriter();
+
+		try{
+
+			String currentUser = userStore.userId
+			log.debug("Adding to: User $currentUser, session ${userStore.sessionId}")
+
+			List bookmarks
+
+			if (body.startsWith('[{')){
+				bookmarks = parseBookmarkJson(body)
+			}else{
+				Document doc = Jsoup.parse(body)
+				Elements elements = doc.body().children().select('bookmarks')
+				if (elements.size() != 1){
+					response.sendError(403, 'Not supported file format. Expected google bookmarks')
+					return
+				}else{
+					bookmarks = parseBookmarksXml(elements[0])
+				}
+			}
+
+			if (currentUser){
+				bookmarksStore.addUserBookmarks(currentUser, bookmarks)
+			}else{
+				bookmarksStore.addSessionBookmarks(userStore.sessionId, bookmarks)
+			}
+
+		}catch (Exception e){
+			e.printStackTrace()
+			response.sendError(403, 'Unable to store file')
+		}
+
+	}
+
+	protected List parseBookmarkJson(String wholeBody){
+		return JacksonHelper.deserialize(wholeBody, List)
+	}
+
+	protected List parseBookmarksXml(Element bookmarkNode){
+		List<Map> result = []
+		int count = 0
+		bookmarkNode.children().each {Element e ->
+			if (e.nodeName() != 'bookmark'){
+				println 'Unknown node '+e.nodeName()
+			}else{
+				count++
+				String id = e.select('id')[0].text()
+				String title = e.select('title')[0].text()
+				String url = e.select('url')[0].text()
+				String timestamp = e.select('timestamp')[0].text()
+
+				Elements labels =  e.select('labels>label')
+				if (labels.size()>1)
+					println "${labels.size()} $url"
+				def bookmark = [id:id, title:title, url:url, timestamp:timestamp, labels: labels*.text()]
+				result << bookmark
+			}
+		}
+
+		println "Total $count bookmarks extracted"
+		return result
+	}
+}
