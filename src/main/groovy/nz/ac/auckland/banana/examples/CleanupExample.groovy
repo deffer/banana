acebb521b88310885977c933aa7916f7c498f8d0
deffer <irina.benediktovich@gmail.com>
2015-04-29 16:32:36 +1200

    add meta tag for mobile support

--- src/main/groovy/nz/ac/auckland/banana/examples/CleanupExample.groovy
+++ src/main/groovy/nz/ac/auckland/banana/examples/CleanupExample.groovy
@@ -2,33 +2,61 @@ package nz.ac.auckland.banana.examples
 
 import nz.ac.auckland.util.JacksonHelper
 
+def filterPrototype = {
+	//String fname = "c:\\apps\\logstash\\prototype_less_original.log"
+	String fname = "c:\\apps\\logstash\\prototype.log"
+	//String newFileName = "c:\\apps\\logstash\\prototype_less_new.log"
+	String newFileName = "c:\\apps\\logstash\\prototype_clean.log"
 
-//String fname = "c:\\apps\\logstash\\prototype_less_original.log"
-String fname = "c:\\apps\\logstash\\prototype.log"
-//String newFileName = "c:\\apps\\logstash\\prototype_less_new.log"
-String newFileName = "c:\\apps\\logstash\\prototype_clean.log"
+	List<String> result = new ArrayList<>();
 
+	new File(fname).readLines().each { String inputLine ->
 
-List<String> result = new ArrayList<>();
+		String line = inputLine.trim()
+		def lineObj = JacksonHelper.deserialize(line, Map.class);
+		def toDelete = lineObj.keySet().findAll { String key ->
+			return (key.startsWith("dim_")
+					|| key.startsWith("fact_")
+					|| key.startsWith("dss_"))
+		}
 
-new File(fname).readLines().each { String inputLine ->
+		toDelete.each { String key ->
+			lineObj.remove(key);
+		}
 
-	String line = inputLine.trim()
-	def lineObj = JacksonHelper.deserialize(line, Map.class);
-	def toDelete = lineObj.keySet().findAll { String key ->
-		return (key.startsWith("dim_")
-		|| key.startsWith("fact_")
-		|| key.startsWith("dss_"))
+		result.add(JacksonHelper.serialize(lineObj));
 	}
 
-	toDelete.each {String key->
-		lineObj.remove(key);
+	File file = new File(newFileName);
+	result.each { String line ->
+		file.append(line + "\n");
+	}
+}
+
+def filterCities(){
+	//String fname = "c:\\apps\\logstash\\prototype_less_original.log"
+	String fname = "c:\\apps\\logstash\\cities15000.txt"
+	//String newFileName = "c:\\apps\\logstash\\prototype_less_new.log"
+	String newFileName = "c:\\apps\\logstash\\cities_clean.txt"
+
+	List<String> result = new ArrayList<>();
+
+	new File(fname).readLines().each { String inputLine ->
+
+		String line = inputLine.trim()
+		if (line.contains("Oak ") || line.contains("Presque Isle") || line.contains("Hillsboro")
+			|| line.contains("Hillsboro") || line.contains("Los ") || line.contains("Miami")
+			|| line.contains("Watsonville") || line.contains('"')
+		){
+			result.add(line);
+		}
 	}
 
-	result.add(JacksonHelper.serialize(lineObj));
+	File file = new File(newFileName);
+	result.each { String line ->
+		file.append(line + "\n");
+	}
 }
 
-File file = new File(newFileName);
-result.each {String line->
-	file.append(line+"\n");
-}
+//filterPrototype();
+filterCities();
